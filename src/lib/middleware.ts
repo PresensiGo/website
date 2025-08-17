30ee28192f490baab3ebb7754be90efd3df4e343
Rizal Anggoro <gnoogler4@gmail.com>
2025-08-17 15:09:18 +0700

    fix: handle refresh token

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -1,3 +1,4 @@
+import { jwtDecode } from "jwt-decode";
 import type { Middleware } from "openapi-fetch";
 import { auth } from "./auth";
 
@@ -5,42 +6,72 @@ export const middleware: Middleware = {
   onRequest: async ({ request }) => {
     const token = auth.get();
     if (token) {
-      request.headers.set("Authorization", `Bearer ${token.accessToken}`);
-    }
-  },
-  onResponse: async ({ response }) => {
-    if (response.status === 401) {
-      const token = auth.get();
-      if (token) {
-        // token expired
-        // mendapatkan token baru
-        try {
-          const refreshTokenResponse = await fetch(
-            `${import.meta.env.VITE_API_BASE_URL}/auth/refresh-token`,
-            {
-              method: "POST",
-              body: JSON.stringify({
-                refresh_token: token.refreshToken,
-              }),
-            }
-          );
+      let accessToken = token.accessToken;
 
-          const status = refreshTokenResponse.status;
-          if (status === 200) {
-            const body = await refreshTokenResponse.json();
-            auth.set({
-              accessToken: body.access_token,
-              refreshToken: body.refresh_token,
-            });
-          } else if (status === 401) {
-            auth.clear();
+      const decoded = jwtDecode(accessToken);
+      const currentTime = Date.now() / 1000;
+
+      if (decoded.exp && decoded.exp < currentTime) {
+        // console.log("access token expired");
+        // masa berlaku token sudah habis
+        const refreshTokenResponse = await fetch(
+          `${import.meta.env.VITE_API_BASE_URL}/auth/refresh-token`,
+          {
+            method: "POST",
+            body: JSON.stringify({
+              refresh_token: token.refreshToken,
+            }),
           }
-        } catch (e) {
-          // refresh token sudah direvoke atau refresh token sudah kadaluarsa
-          // clear auth untuk memaksa user login kembali
-          auth.clear();
+        );
+        const status = refreshTokenResponse.status;
+
+        if (status === 200) {
+          const body = await refreshTokenResponse.json();
+          auth.set({
+            accessToken: body.access_token,
+            refreshToken: body.refresh_token,
+          });
+
+          accessToken = body.access_token;
         }
       }
+
+      request.headers.set("Authorization", `Bearer ${accessToken}`);
     }
   },
+  // onResponse: async ({ response }) => {
+  //   if (response.status === 401) {
+  //     const token = auth.get();
+  //     if (token) {
+  //       // token expired
+  //       // mendapatkan token baru
+  //       try {
+  //         const refreshTokenResponse = await fetch(
+  //           `${import.meta.env.VITE_API_BASE_URL}/auth/refresh-token`,
+  //           {
+  //             method: "POST",
+  //             body: JSON.stringify({
+  //               refresh_token: token.refreshToken,
+  //             }),
+  //           }
+  //         );
+
+  //         const status = refreshTokenResponse.status;
+  //         if (status === 200) {
+  //           const body = await refreshTokenResponse.json();
+  //           auth.set({
+  //             accessToken: body.access_token,
+  //             refreshToken: body.refresh_token,
+  //           });
+  //         } else if (status === 401) {
+  //           auth.clear();
+  //         }
+  //       } catch (e) {
+  //         // refresh token sudah direvoke atau refresh token sudah kadaluarsa
+  //         // clear auth untuk memaksa user login kembali
+  //         auth.clear();
+  //       }
+  //     }
+  //   }
+  // },
 };
