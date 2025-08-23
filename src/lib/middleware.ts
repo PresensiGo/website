544d329074ea7c975368fb5ef19a6d518d82cf35
Rizal Anggoro <gnoogler4@gmail.com>
2025-08-23 18:38:17 +0700

    fix: token refreshing

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -22,7 +22,7 @@ const refreshToken = async (token: { refreshToken: string }) => {
           body: JSON.stringify({
             refresh_token: token.refreshToken,
           }),
-        }
+        },
       );
       const status = refreshTokenResponse.status;
 
@@ -33,12 +33,12 @@ const refreshToken = async (token: { refreshToken: string }) => {
           refreshToken: body.refresh_token,
         });
         resolve();
-      } else if (status === 401) {
+      } else if (status === 403) {
         auth.clear();
         reject(new Error("Refresh token expired or invalid"));
       }
     } catch (e) {
-      auth.clear();
+      // auth.clear();
       reject(e);
     } finally {
       isRefreshing = false;
@@ -75,39 +75,4 @@ export const middleware: Middleware = {
       request.headers.set("Authorization", `Bearer ${accessToken}`);
     }
   },
-  // onResponse: async ({ response }) => {
-  //   if (response.status === 401) {
-  //     const token = auth.get();
-  //     if (token) {
-  //       // token expired
-  //       // mendapatkan token baru
-  //       try {
-  //         const refreshTokenResponse = await fetch(
-  //           `${import.meta.env.VITE_API_BASE_URL}/auth/refresh-token`,
-  //           {
-  //             method: "POST",
-  //             body: JSON.stringify({
-  //               refresh_token: token.refreshToken,
-  //             }),
-  //           }
-  //         );
-
-  //         const status = refreshTokenResponse.status;
-  //         if (status === 200) {
-  //           const body = await refreshTokenResponse.json();
-  //           auth.set({
-  //             accessToken: body.access_token,
-  //             refreshToken: body.refresh_token,
-  //           });
-  //         } else if (status === 401) {
-  //           auth.clear();
-  //         }
-  //       } catch (e) {
-  //         // refresh token sudah direvoke atau refresh token sudah kadaluarsa
-  //         // clear auth untuk memaksa user login kembali
-  //         auth.clear();
-  //       }
-  //     }
-  //   }
-  // },
 };
